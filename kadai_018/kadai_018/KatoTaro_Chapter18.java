7f3b1a87532c42a0ed23dd8d3913dca4c4141a2a
yamanaka-isus <yamanaka@i-sus.com>
2025-09-22 11:07:37 +0900

    Add files via upload

---
+++ kadai_018/kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18{
+
+	public void setGivenName(String givenName) {
+
+		this.givenName = givenName;
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("私はJavaが得意です\n");
+	}
+
+	
+}
