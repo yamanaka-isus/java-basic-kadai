9010d6c1dfb48cbfc3dbc4f93a98339888a912ba
yamanaka-isus <yamanaka@i-sus.com>
2025-09-22 17:28:37 +0900

    文字列の文言修正。

--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -5,44 +5,30 @@ import java.util.List;
 
 public class Dictionary_Chapter21 {
 
-	HashMap<String,String> dic = new HashMap<String, String>();
-	
+	HashMap<String, String> dic = new HashMap<String, String>();
+
 	public Dictionary_Chapter21() { //要素を追加するメソッド
-	
-	dic.put("apple","りんご");
-	dic.put("peach","桃");
-	dic.put("banana","バナナ");
-	dic.put("lemon","レモン");
-	dic.put("pear","梨");
-	dic.put("kiwi","キウィ");
-	dic.put("strawberry","いちご");
-	dic.put("grape","ぶどう");
-	dic.put("muscut","マスカット");
-	dic.put("cherry","さくらんぼ");
+
+		dic.put("apple", "りんご");
+		dic.put("peach", "桃");
+		dic.put("banana", "バナナ");
+		dic.put("lemon", "レモン");
+		dic.put("pear", "梨");
+		dic.put("kiwi", "キウィ");
+		dic.put("strawberry", "いちご");
+		dic.put("grape", "ぶどう");
+		dic.put("muscut", "マスカット");
+		dic.put("cherry", "さくらんぼ");
 	}
-	
-	public void searchByKeys(List<String>keys) { //String型のリストをkeysとして受け取る
+
+	public void searchByKeys(List<String> keys) { //String型のリストをkeysとして受け取る
 		for (String key : keys) { //受け取ったkeysをkeyへ代入してループ
 			if (dic.containsKey(key)) { //Map(dic)に存在するか判定
-				System.out.println(key + "の日本語:" + dic.get(key));
+				System.out.println(key + "の意味は" + dic.get(key));
 			} else {
-				System.out.println(key + "は辞書に存在していません");
+				System.out.println(key + "は辞書に存在しません");
 			}
 		}
 	}
 
-
-
 }
-
-
-/*
-	public class checkMap() {
-		if (map.isEmpty()) {
-			System.out.println("要素はありません");
-		}else {
-		for (String key : dic.keySet()) {
-			//各キーごとに処理
-			System.out.println(key + "の意味は" + dic.get(key));
-		}
-*/
